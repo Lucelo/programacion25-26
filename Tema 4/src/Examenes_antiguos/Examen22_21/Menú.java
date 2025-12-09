b766250a359b80ef2b8ba9f965412b6cf45614df
Lucelo <zeroestrellasazules@gmail.com>
2025-12-09 14:23:09 +0100

    09/12/25

---
+++ Tema 4/src/Examenes_antiguos/Examen22_21/Menú.java
@@ -0,0 +1,259 @@
+package Examen22_21;
+
+import java.util.Scanner;
+
+public class Menú {
+
+    Películas[] biblioteca = new Películas[20];
+    int numpelis = 0;
+
+    Scanner sc = new Scanner(System.in);
+
+    public void main(String[] args) {
+
+        System.out.println("1. Añadir una nueva película");
+        System.out.println("2. Consultar la biblioteca");
+        System.out.println("3. Buscar películas por etiquetas");
+        System.out.println("4. Buscar película por título");
+        System.out.println("5. Buscar películas con presupuesto máximo");
+        System.out.println("6. Salir");
+
+        boolean salir = false;
+
+        pelisbasicas();
+
+        while (!salir) {
+            System.out.print("elecion: ");
+
+            int elec = sc.nextInt();
+            sc.nextLine();
+
+
+            switch (elec) {
+                //
+                case 1 -> {
+
+
+                    if (numpelis < biblioteca.length) {
+                        añadirpeli();
+                        System.out.println("Película añadida añadida.");
+                    } else {
+                        System.out.println("No se pueden añadir más (máximo 20).");
+                    }
+
+
+                }
+                //
+                case 2 -> {
+
+                    mostrarBiblioteca();
+
+                }
+                //
+                case 3 -> {
+
+                    etiquetas();
+
+                    System.out.print("Por que etiqueta desea buscar: ");
+                    String busq = sc.nextLine();
+
+                    for (int i = 0; i < numpelis; i++) {
+                        if (biblioteca[i].etiquetas.indexOf(busq) != -1) {
+                            System.out.println(biblioteca[i].titulo);
+
+                        }
+
+                    }
+
+                }
+                //
+                case 4 -> {
+                    System.out.print("Nombre de la pelicula: ");
+                    String busq = sc.nextLine();
+
+                    for (int i = 0; i < numpelis; i++) {
+                        if (biblioteca[i].titulo.contains(busq)) {
+                            datos(i);
+                        }
+                    }
+
+                }
+                //
+                case 5 -> {
+
+                    double max = sc.nextDouble();
+
+                    for (int i = 0; i < numpelis; i++) {
+                        if (biblioteca[i].presupuesto < max) {
+
+                            System.out.println(biblioteca[i].titulo);
+
+                        }
+
+
+                    }
+
+
+                }
+                //
+                case 6 -> salir = true;
+
+                default -> System.out.println("La opción no es valida");
+
+            }
+
+        }
+
+    }
+
+    private void datos(int i) {
+        System.out.println("Su nombre es " + biblioteca[i].titulo + " se estreno en " + biblioteca[i].anoDeEstreno + " bajo el mando de " + biblioteca[i].director);
+        System.out.println("Tuvo un presupuesto de " + biblioteca[i].presupuesto + " y una recaudacion de " + biblioteca[i].recaudacion);
+        System.out.println("Su sinopsis es " + biblioteca[i].sinopsis + " bajo las etiquetas de " + biblioteca[i].etiquetas);
+    }
+
+    private void etiquetas() {
+        int salto = 0;
+
+        System.out.println("Etiquetas");
+
+        for (int i = 0; i < numpelis; i++) {
+
+            if (biblioteca[i] != null) {
+
+
+                System.out.print(biblioteca[i].etiquetas + ", ");
+
+                if (salto == 2) {
+
+                    System.out.println();
+
+                    salto = 0;
+
+                } else {
+
+
+                    salto++;
+                }
+
+            }
+
+        }
+
+        System.out.println();
+
+    }
+
+    void pelisbasicas() {
+        biblioteca[numpelis++] = new Películas(
+                "El Viaje Estelar",
+                "Juan",
+                2014,
+                120000000,
+                700000000,
+                "Una misión espacial que cambia el destino de la humanidad.",
+                new StringBuilder("ciencia ficción, espacio, aventura")
+        );
+
+        biblioteca[numpelis++] = new Películas(
+                "Sombras en la Ciudad",
+                "Juan",
+                2020,
+                20000000,
+                85000000,
+                "Un detective debe resolver una serie de crímenes misteriosos.",
+                new StringBuilder("thriller, crimen, misterio")
+        );
+
+        biblioteca[numpelis++] = new Películas(
+                "El Reino Perdido",
+                "Juan",
+                2018,
+                200000000,
+                950000000,
+                "Una épica batalla en un mundo de fantasía antiguo.",
+                new StringBuilder("fantasía, acción, épica")
+        );
+
+        biblioteca[numpelis++] = new Películas(
+                "Corazones de Invierno",
+                "Juan",
+                2012,
+                15000000,
+                40000000,
+                "Una historia íntima sobre dos personas que se encuentran por destino.",
+                new StringBuilder("romance, drama")
+        );
+
+        biblioteca[numpelis++] = new Películas(
+                "El Último Hacker",
+                "Juan",
+                2023,
+                8000000,
+                50000000,
+                "Un programador descubre una conspiración global detrás de la red.",
+                new StringBuilder("tecnología, hacker, suspense")
+        );
+
+    }
+
+    void añadirpeli() {
+        sc.nextLine();
+
+        System.out.println("titulo:");
+        String titulo = sc.nextLine();
+
+        System.out.println("anoDeEstreno:");
+        int anoDeEstreno = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("director:");
+        String director = sc.nextLine();
+
+        System.out.println("presupuesto:");
+        double presupuesto = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.println("recaudacion:");
+        double recaudacion = sc.nextDouble();
+        sc.nextLine();
+
+        System.out.println("sinopsis:");
+        String sinopsis = sc.nextLine();
+
+        System.out.println("Cantidad de etiquetas:");
+
+        int canetiq = sc.nextInt();
+        sc.nextLine();
+
+        StringBuilder etiquetas = new StringBuilder();
+
+        for (int i = 0; i < canetiq; i++) {
+            System.out.println("Etiqueta " + i + ": ");
+
+            String etiqueta = sc.nextLine();
+
+            etiquetas.append(etiqueta).append(", ");
+
+        }
+
+        biblioteca[numpelis] = new Películas(titulo, director, anoDeEstreno, presupuesto, recaudacion, sinopsis, etiquetas);
+
+        numpelis++;
+
+    }
+
+    public void mostrarBiblioteca() {
+        for (int i = 0; i < biblioteca.length; i++) {
+            if (biblioteca[i] != null) {
+
+                System.out.println((i + 1));
+                datos(i);
+                System.out.println("Su valoracion es de " + biblioteca[i].valoracion());
+
+            }
+        }
+    }
+
+
+}
---
+++ Tema 4/src/Examenes_antiguos/Examen22_21/Películas.java
@@ -0,0 +1,58 @@
+package Examen22_21;
+
+public class Películas {
+
+    public String titulo;
+
+    public int anoDeEstreno;
+
+    public String director;
+
+    public double presupuesto;
+
+    public double recaudacion;
+
+    public String sinopsis;
+
+    public StringBuilder etiquetas;
+
+    public double valoracion;
+
+    Películas(String título,
+              String director,
+              int añoDeEstreno,
+              double Presupuesto,
+              double Recaudación,
+              String sinopsis,
+              StringBuilder etiquetas
+    ) {
+
+        this.titulo = título;
+        this.director=director;
+        this.anoDeEstreno = añoDeEstreno;
+        this.presupuesto = Presupuesto;
+        this.recaudacion = Recaudación;
+        this.sinopsis = sinopsis;
+        this.etiquetas = etiquetas;
+
+    }
+
+
+    double valoracion() {
+
+        if (presupuesto < recaudacion) {
+            return valoracion = 10;
+
+        } else if (presupuesto > recaudacion) {
+
+            return valoracion = 0;
+
+        } else {
+            return valoracion = 5;
+
+        }
+
+    }
+
+
+}
